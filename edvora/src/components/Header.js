61dd0900efbe22a789881c26ac6d2e008ce56c45
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-02-06 23:15:21 +0530

    Divided the homepage in SideBar and Main Content

---
+++ edvora/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/header.css'
+const Header = () => {
+  return (
+  <div className='header'>
+      <h1>Edvora</h1>
+      <h3>Products</h3>
+      <h4>Product Name</h4>
+      <hr />
+  </div>
+  );
+};
+
+export default Header;
