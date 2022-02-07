a4a9b88517419efa8345d501ff91e0e5f047fa9f
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-02-07 12:48:49 +0530

    Made the sideBar

--- edvora/src/components/Sidebar.js
+++ edvora/src/components/Sidebar.js
@@ -4,8 +4,13 @@ import '../css/sidebar.css';
 const Sidebar = () => {
   return (
   <div className='sidebar-container'>
-      
-  </div>);
+      <h4>Filter</h4>
+      <hr />
+      <div className='product-div'>Products<div className='dropdown-arrow'></div></div>
+      <div className='product-div'>State<div className='dropdown-arrow'></div></div>
+      <div className='product-div'>City<div className='dropdown-arrow'></div></div>
+  </div>
+  );
 };
 
 export default Sidebar;
