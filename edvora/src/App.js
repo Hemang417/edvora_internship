8f6253b23c56e9c8349e0597c7bbb52fb6ec04ae
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-02-07 11:49:19 +0530

    Added SideBar to the HomePage

--- edvora/src/App.js
+++ edvora/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import Container from './components/Container';
 import Header from './components/Header'
-
+import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className='homepage-background'>
       <div className='sidebar'>
-        
+        <Sidebar />
       </div>
       <div className='main-content'>
         <Header />
--- edvora/src/components/Sidebar.js
+++ edvora/src/components/Sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import '../css/sidebar.css';
 
 const Sidebar = () => {
   return (
-  <div className='sidebar'>
-
+  <div className='sidebar-container'>
+      
   </div>);
 };
 
