7cefc7ac176f9861164bd63f910c144505bf374e
Hemang Ranjan <74303960+Hemang417@users.noreply.github.com>
2022-02-09 23:20:57 +0530

    Api Added
    Value cannot be seen on front-end

--- edvora/src/components/Container.js
+++ edvora/src/components/Container.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import '../css/container.css'
 const Container = () => {
-  return (
-      <div>
-          <h5>Product Name</h5>
-      <hr />
-          <div className='container'>
+    const [products, setProducts] = useState([]);
 
-      </div>
-  </div>
-  );
+    useEffect(()=> {
+        const getProducts = async() => {
+            // const res = await fetch('https://newsapi.org/v2/top-headlines?apiKey=f689852284a84d42ae0ee17cc3d7f0a0')
+            const res = await fetch('https://assessment-edvora.herokuapp.com/')
+
+            const data = await res.json();
+            console.log(data)
+            return data
+        }
+        getProducts().then(data => setProducts(data))
+        .catch(err => console.log(err));
+    },[])   
+                return (
+                    <div>
+                        <h5>Product Name</h5>
+                    <hr />
+                        <div className='container'>
+                        {(products.products !== undefined) ? products.products.map((elem) => {
+                            <div className='product-container-div'>{elem.product_name}</div>
+                        }): console.log("error while fetching data")}
+                    </div>
+                </div>
+                )
+            
 };
+    
 
 export default Container;
